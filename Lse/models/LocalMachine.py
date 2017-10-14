3eec13ff7c631b2a641b3d625c6adc31ec48da93
Ivan Ponomarev <ivan.ponomarev.pi@gmail.com>
2017-10-14 14:19:09 +0200

    Some small fixes

--- Lse/models/LocalMachine.py
+++ Lse/models/LocalMachine.py
@@ -6,6 +6,8 @@ import datetime
 import platform
 import socket
 
+from pathlib import Path
+
 from Lse.models import AbstractMachine
 
 
@@ -45,12 +47,19 @@ class LocalMachine(AbstractMachine):
     @staticmethod
     def _getvaluefromlsb(compared):
         res = ""
-        f = open("/etc/lsb-release", "r")
-        for line in f.readlines():
-            key, val = line.replace("\n", "").replace("\r", "").split('=')
-            if key == compared:
-                res = val.replace("\"", "")
-        f.close()
+        lsb_path = "/etc/lsb-release"
+
+        if Path(lsb_path).is_file():
+            f = open(lsb_path, "r")
+            for line in f.readlines():
+                key, val = line.replace("\n", "").replace("\r", "").split('=')
+                if key == compared:
+                    res = val.replace("\"", "")
+            f.close()
+        else:
+            res = "LSB File is not found: \"%s\"" % lsb_path
+
+
         return res
 
     @property
@@ -62,13 +71,13 @@ class LocalMachine(AbstractMachine):
         return self._getvaluefromlsb("DISTRIB_ID")
 
     def __str__(self):
-        _substr = ""
-        _substr += "Hostname:\t\t" + self.hostname + "\n"
-        _substr += "OS:\t\t\t\t" + self.os + " (" + self.distribId + ")" + "\n"
-        _substr += "Distrib name:\t" + self.distribName + "\n"
-        _substr += "Version:\t\t" + self.version + "\n"
-        _substr += "Architecture:\t" + self.architecture + "\n"
-        _substr += "Platform:\t\t" + self.platform + "\n"
-        _substr += "DateTime:\t\t" + str(self.datetime) + "\n"
-        return _substr
+        substr = ""
+        substr += "Hostname:\t\t" + self.hostname + "\n"
+        substr += "OS:\t\t\t\t" + self.os + " (" + self.distribId + ")" + "\n"
+        substr += "Distrib name:\t" + self.distribName + "\n"
+        substr += "Version:\t\t" + self.version + "\n"
+        substr += "Architecture:\t" + self.architecture + "\n"
+        substr += "Platform:\t\t" + self.platform + "\n"
+        substr += "DateTime:\t\t" + str(self.datetime) + "\n"
+        return substr
 
